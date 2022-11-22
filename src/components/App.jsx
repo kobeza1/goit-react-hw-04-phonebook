ca9fd98027f82c130339a341e64de66b0155739e
kobeza1 <102040962+kobeza1@users.noreply.github.com>
2022-11-22 20:07:45 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import Form from './Form/Form';
 import { Box } from './App.styled';
